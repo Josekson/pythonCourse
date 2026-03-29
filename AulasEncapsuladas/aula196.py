a77628661f5c558beb27edd6812415c030d2dd0f
Josekson <138631997+Josekson@users.noreply.github.com>
2026-03-29 19:47:24 -0300

    Delete AulasEncapsuladas directory

--- AulasEncapsuladas/aula196.py
+++
@@ -1,3 +0,0 @@
-def soma(a,b,/):
-    print(a+b)
-soma(a=1,b=3)
--- AulasEncapsuladas/aula220.py
+++
@@ -1,6 +0,0 @@
-# Associação - usa um objeto dentro de outro objeto
-# Agregação - tem um objeto dentro de outro objeto
-# Composição - É dono de um objeto
-
-#Heranca - Quando uma classe herda outra classe (herda atributos e métodos)
-
